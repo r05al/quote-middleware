cc102d9f598dc7baf57bbf89249d971b0ff56f7b
Peter Rosal <peter.rosal@gmail.com>
2016-01-08 18:48:06 -0500

    Create conditional using env path info

--- ricky_g_says.rb
+++ ricky_g_says.rb
@@ -4,8 +4,20 @@ class RickyGSays
   end
 
   def call(env)
-    status, headers, response = @app.call(env)
-    response.push(" -Ricky Gervais")
-    [status, headers, response]
+    if env['PATH_INFO'] == '/quote'
+      request = Rack::Request.new(env)
+      case request.request_method
+      when 'GET'
+        status, headers, response = @app.call(env)
+        response.push(" -Ricky Gervais")
+        [status, headers, response]
+      when 'POST'
+        [404, {}, [""]]
+      else
+        @app.call(env)
+      end
+    else
+      @app.call(env)
+    end
   end
 end
